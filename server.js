499e21b1c044d49e76865d009c49f49ba9591865
Swaraj Sambare <ssambare11111@gmail.com>
2025-05-19 11:06:12 -0400

    Fix contact form to work with Vercel serverless functions

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
@@ -64,3 +65,4 @@ router.post("/contact", (req, res) => {
     }
   });
 });
+*/
