2520459094cbfd3566855c91e12d1b482e40c141
Swaraj Sambare <ssambare11111@gmail.com>
2025-05-20 15:47:33 -0400

    Added Resume Updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,21 +8,20 @@ const Resume = () => {
       <Container className="text-center py-5">
         <h2 className="mb-4">My Resume</h2>
         
-        <iframe
-  src="/Swaraj_Sambare_Resume.pdf"
-  width="100%"
-  height="600px"
-  title="Resume Preview"
-  style={{ border: "1px solid #ccc", borderRadius: "8px" }}
-></iframe>
+       <iframe
+        src={`${process.env.PUBLIC_URL}/Swaraj_Sambare_Resume.pdf`}
+        width="100%"
+        height="600px"
+        title="Resume Preview"
+        style={{ border: "1px solid #ccc", borderRadius: "8px" }}
+        ></iframe>
 
-
-        <div className="mt-4">
-          <a
-            href="/Swaraj_Sambare_Resume.pdf"
-            download="Swaraj_Sambare_Resume.pdf"
-            className="btn btn-primary"
-          >
+<div className="mt-4">
+  <a
+    href={`${process.env.PUBLIC_URL}/Swaraj_Sambare_Resume.pdf`}
+    download="Swaraj_Sambare_Resume.pdf"
+    className="btn btn-primary"
+  >
             Download Resume
           </a>
         </div>
