106233157eedc0264c2b58fd30387cc3dd70b1a6
Swaraj Sambare <ssambare11111@gmail.com>
2025-05-20 15:39:58 -0400

    Added Resume

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Footer } from './components/Footer';
 import SplashScreen from './components/SplashScreen';
-import { Resume } from './components/Resume'; 
+import Resume from './components/Resume'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
